9dac2f181d3e31e2ff48fdac07049b2f02213a09
Velmoren <velmoren@yahoo.com>
2019-11-08 17:54:31 +0300

    lesson12

---
+++ social-project/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://image.flaticon.com/icons/svg/174/174855.svg" alt="logo" />
+        </header>
+    )
+}
+
+export default Header;
